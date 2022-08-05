10b459219c22e64f01ac26bb8cb8282ddc67507d
john <john.davis.2015.03@gmail.com>
2022-08-04 22:13:03 -0500

    Fin de la Sección 19

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -12,38 +12,51 @@ var usuario = {
   sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function () {
   console.log('Conectado al servidor');
 
   socket.emit('entrarChat', usuario, function (resp) {
-    console.log('Usuarios conectados', resp);
+    // console.log('Usuarios conectados', resp);
+    renderizarUsuarios(resp);
   });
+
 });
 
 // escuchar
 socket.on('disconnect', function () {
+
   console.log('Perdimos conexión con el servidor');
+
 });
+
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//   nombre: 'Fernando',
-//   mensaje: 'Hola Mundo'
-// }, function (resp) {
-//   console.log('respuesta server: ', resp);
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
 // });
 
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-  console.log('Servidor:', mensaje);
+  // console.log('Servidor:', mensaje);
+  renderizarMensajes(mensaje, false);
+  scrollBottom();
 });
 
-// Escuchar cambios de usuarios cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function (personas) {
-  console.log(personas);
+  // console.log(personas);
+  renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function (mensaje) {
-  console.log('Mensaje Privado', mensaje);
-})
+
+  console.log('Mensaje Privado:', mensaje);
+
+});
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -8,25 +8,24 @@ io.on('connection', (client) => {
 
   client.on('entrarChat', (data, callback) => {
     // console.log(data);
-
     if (!data.nombre || !data.sala) {
       return callback({
         error: true,
         mensaje: 'El nombre/sala es necesario'
       })
     }
-
     client.join(data.sala);
-
     usuarios.agregarPersona(client.id, data.nombre, data.sala);
     client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+    client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${data.nombre} se unió`));
     callback(usuarios.getPersonasPorSala(data.sala));
   });
 
-  client.on('crearMensaje', (data) => {
+  client.on('crearMensaje', (data, callback) => {
     let persona = usuarios.getPersona(client.id);
     let mensaje = crearMensaje(persona.nombre, data.mensaje);
     client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    callback(mensaje);
   })
 
   client.on('disconnect', () => {
